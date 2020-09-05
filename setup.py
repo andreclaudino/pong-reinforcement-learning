7656c9361c354d41e05e907b4c9f8f44c7221953
André Claudino <andre.claudino@b2wdigital.com>
2020-09-04 23:33:40 -0300

    Start python project

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pong-service',
+    version='1.0.0',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='Time IA-FRONT',
+    author_email='',
+    description='',
+    install_requires = [
+        "FLASK-JSON==0.3.4",
+        "flask==1.1.2",
+        "click==7.1.2",
+        "tensorflow==2.3.0"
+    ]
+)
